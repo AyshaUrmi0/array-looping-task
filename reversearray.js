c970228e3a42b84d2188f5016d39734410473ec2
AyshaUrmi0 <ayshaismail021@gmail.com>
2024-09-04 00:41:12 +0900

    reversearray done

--- reversearray.js
+++ reversearray.js
@@ -1,13 +1,36 @@
 // Task 1
 // Write a JavaScript code to reverse the array colors without using the reverse method.
 
-// Input: const colors = ['red', 'blue', 'green', 'yellow', 'orange']
+// method-1
 
-// Output:
+const array=['red', 'blue', 'green', 'yellow', 'orange'];
 
-// ['orange', 'yellow', 'green', 'blue', 'red']
+const revarray=[];
 
-cont array=['red', 'blue', 'green', 'yellow', 'orange'];
+for(const element of array){
+    revarray.unshift(element);
+    
+}
+console.log(revarray);
 
-let reversearray=array.reverse();
-console.log(reversearray);
+
+// method-2
+
+const array3=['red', 'blue', 'green', 'yellow', 'orange'];
+
+const revarray1=[];
+for(let i=array.length-1;i>=0;i--){
+    revarray1.push(array[i]);
+}
+
+console.log(revarray1);
+
+// method-3
+
+const array4=['red', 'blue', 'green', 'yellow', 'orange'];
+
+const revarray2=array4.reverse();
+console.log(revarray2);
+
+
+
