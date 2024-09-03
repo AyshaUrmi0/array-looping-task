baa0b7febd25205af7555b71d7c46253f9d59471
AyshaUrmi0 <ayshaismail021@gmail.com>
2024-09-04 00:29:55 +0900

    file name changed

---
+++ reversearray.js
@@ -0,0 +1,13 @@
+// Task 1
+// Write a JavaScript code to reverse the array colors without using the reverse method.
+
+// Input: const colors = ['red', 'blue', 'green', 'yellow', 'orange']
+
+// Output:
+
+// ['orange', 'yellow', 'green', 'blue', 'red']
+
+cont array=['red', 'blue', 'green', 'yellow', 'orange'];
+
+let reversearray=array.reverse();
+console.log(reversearray);
